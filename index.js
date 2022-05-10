a1889922c6ad55ce41634c961841e52a0ca4cc6b
sakilshikdar <sakilshikdar2504@gmail.com>
2022-05-10 13:19:58 +0600

    delete-complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient } = require("mongodb")
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const app = express();
 
@@ -10,23 +11,59 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fsloq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
-const run = async () => {
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
     try {
         await client.connect();
         const productCollection = client.db('wearHouse2').collection('product')
-        app.get('/product', async(req, res)=>{
+
+        //get users
+        app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
+        })
+
+
+        //get one user
+        app.get("/inventory/:id", async (req, res) => {
+            const id = req.params;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        });
+
+        // update user
+        app.put('/inventory/:id', async (req, res) => {
+            const id = (req.params.id);
+            const updatedNum = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedNum.quantityNum
+                }
+            };
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+
+        });
+
+        //delete item
+        app.delete('/items/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
         })
     }
     catch (error) {
         console.log(error);
     }
 }
-run()
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log('Listening to port', port)
